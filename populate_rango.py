ce523a408a97ec49b54932c089debee966b9b18d
Joe Dunford Wood <2755328d@student.gla.ac.uk>
2024-02-01 17:04:40 +0000

    Section 6 Complete

--- populate_rango.py
+++ populate_rango.py
@@ -4,6 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.setti
 import django
 django.setup()
 from rango.models import Category, Page
+import random
 
 def populate():
     # First, we will create lists of dictionaries containing the pages
@@ -44,7 +45,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat,cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],random.randint(0,100))
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
